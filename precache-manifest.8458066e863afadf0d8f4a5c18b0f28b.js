b19f26bb4d60463faa9d110222455ea64ff93869
Reshandre <katz.andre@gmail.com>
2022-07-11 19:47:57 +0200

    Updates

---
+++ precache-manifest.8458066e863afadf0d8f4a5c18b0f28b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "72fa3fe48138ab0e1f62",
+    "url": "/gh-pages-react/static/css/main.bf31c1fa.chunk.css"
+  },
+  {
+    "revision": "72fa3fe48138ab0e1f62",
+    "url": "/gh-pages-react/static/js/main.72fa3fe4.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/gh-pages-react/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "37d6c4bb6a1d513552a3",
+    "url": "/gh-pages-react/static/js/2.37d6c4bb.chunk.js"
+  },
+  {
+    "revision": "2b8b0207f5356936866b",
+    "url": "/gh-pages-react/static/js/runtime~main.2b8b0207.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/gh-pages-react/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "45ac9edc1ef3d420b2c364c45491bd26",
+    "url": "/gh-pages-react/index.html"
+  }
+];
